e7cda256ded4211595d3000d78856df753764300
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-05-21 13:16:01 -0300

    Atividade Concluída

--- app/src/main/java/com/example/app_sqlite/MainActivity.kt
+++ app/src/main/java/com/example/app_sqlite/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 
 import android.database.sqlite.SQLiteDatabase
 import android.util.Log
+import android.widget.TextView
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
@@ -19,41 +20,51 @@ class MainActivity : AppCompatActivity() {
         try {
             val bd = openOrCreateDatabase("app", MODE_PRIVATE, null)
 
-            //bd.execSQL("DELETE FROM pessoa")
+            //Excluir a tabela aluno
+            bd.execSQL("DROP TABLE IF EXISTS aluno")
 
-            bd.execSQL("CREATE TABLE IF NOT EXISTS pessoa (nome VARCHAR, idade INT(3))")
 
+            bd.execSQL("CREATE TABLE IF NOT EXISTS aluno (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(50), dataNascimento DATE, ra VARCHAR(13), curso VARCHAR(40), turma VARCHAR(20), turno VARCHAR(20))")
 
-            bd.execSQL("UPDATE pessoa SET nome = 'Marcos Vasconcelos de Oliveira' WHERE nome = 'Marcos'")
+            //bd.execSQL("DROP TABLE aluno")
 
-            bd.execSQL("DROP TABLE pessoa")
-            bd.execSQL("CREATE TABLE IF NOT EXISTS pessoa (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INT(3))")
+            bd.execSQL("INSERT INTO aluno (nome, dataNascimento, ra, curso, turma, turno) VALUES ('José', '2000-03-09', '1234567891011', 'DSM', 'DSM2', 'Manhã')")
+            bd.execSQL("INSERT INTO aluno (nome, dataNascimento, ra, curso, turma, turno) VALUES ('Pedro Costa', '2003-04-11', '1110987654321', 'DSM', 'DSM1', 'Noite')")
+            bd.execSQL("INSERT INTO aluno (nome, dataNascimento, ra, curso, turma, turno) VALUES ('Maria Oliveira', '2002-05-10', '1234567891011', 'DSM', 'DSM2', 'Manhã')")
+            bd.execSQL("INSERT INTO aluno (nome, dataNascimento, ra, curso, turma, turno) VALUES ('João Santos', '2001-06-12', '4002892270707', 'DSM', 'DSM1', 'Noite')")
+            bd.execSQL("INSERT INTO aluno (nome, dataNascimento, ra, curso, turma, turno) VALUES ('Ana Souza', '2004-07-13', '0800707070707', 'DSM', 'DSM2', 'Manhã')")
+            bd.execSQL("INSERT INTO aluno (nome, dataNascimento, ra, curso, turma, turno) VALUES ('Carlos Pereira', '2005-08-14', '5963269872014', 'DSM', 'DSM1', 'Noite')")
+            bd.execSQL("INSERT INTO aluno (nome, dataNascimento, ra, curso, turma, turno) VALUES ('Marta', '2006-09-15', '6354793200651', 'DSM', 'DSM2', 'Manhã')")
+            bd.execSQL("INSERT INTO aluno (nome, dataNascimento, ra, curso, turma, turno) VALUES ('Ricardo Santos', '2002-10-16', '1234567891011', 'DSM', 'DSM1', 'Noite')")
+            bd.execSQL("INSERT INTO aluno (nome, dataNascimento, ra, curso, turma, turno) VALUES ('Henrique Bonifácio', '2000-11-17', '1110987654321', 'DSM', 'DSM2', 'Manhã')")
+            bd.execSQL("INSERT INTO aluno (nome, dataNascimento, ra, curso, turma, turno) VALUES ('Angela Lopes', '1999-12-18', '1234567891011', 'DSM', 'DSM5', 'Noite')")
 
-            bd.execSQL("INSERT INTO pessoa (nome, idade) VALUES ('Marcos', 18)")
-            bd.execSQL("INSERT INTO pessoa (nome, idade) VALUES ('Fatec Diadema', 20)")
-            bd.execSQL("INSERT INTO pessoa (nome, idade) VALUES ('Maria', 25)")
+            bd.execSQL("UPDATE aluno SET nome = 'José da Silva' WHERE nome = 'José'")
+            bd.execSQL("UPDATE aluno SET nome = 'Marta Ribeiro' WHERE nome = 'Marta'")
+            bd.execSQL("UPDATE aluno SET curso = 'ADS', turma = 'ADS2' WHERE nome = 'Henrique Bonifácio'")
 
+            val outputTextView = findViewById<TextView>(R.id.bancoDeDados)
+            val stringBuilder = StringBuilder()
 
             //val cursor = bd.rawQuery("SELECT id, nome, idade FROM pessoa ORDER BY nome", null)
-            val cursor = bd.rawQuery("SELECT * FROM pessoa ORDER BY nome", null)
+            val cursor = bd.rawQuery("SELECT * FROM aluno ORDER BY nome", null)
 
             val indiceId = cursor.getColumnIndex("id")
             val indiceNome = cursor!!.getColumnIndex("nome")
-            val indiceIdade = cursor!!.getColumnIndex("idade")
+            val indiceCurso = cursor!!.getColumnIndex("curso")
 
             cursor.moveToFirst()
-            while(cursor != null) {
-                /*
-
-                Log.i("RESULTADO = NOME: ", cursor.getString(0))
-                Log.i("RESULTADO = NOME: ", cursor.getString(indiceNome))
-                Log.i("RESULTADO = IDADE: ", cursor.getString(indiceIdade))
-
-                 */
-
-                Log.i("RESULTADO - ID: ", cursor.getString(indiceId) + "NOME: " + cursor.getString(indiceNome) + "IDADE: " + cursor.getString(indiceIdade))
+            while(!cursor.isAfterLast) {
+                val id = cursor.getInt(indiceId)
+                val nome = cursor.getString(indiceNome)
+                val curso = cursor.getString(indiceCurso)
+                stringBuilder.append("ID: $id, Nome: $nome, Curso: $curso\n")
                 cursor.moveToNext()
             }
+
+            outputTextView.text = stringBuilder.toString()
+
+            cursor.close()
         } catch (e: Exception) {
             e.printStackTrace()
         }
